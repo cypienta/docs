102cce75ed39aafdff6b855d60fdf819973b11d3
Nidheesh <nilu.1999@yahoo.com>
2024-11-15 15:20:06 +0000

    version update

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -6,8 +6,10 @@ project = 'Cypienta'
 copyright = '2024, Cypienta'
 author = 'Cypienta'
 
-release = '1.0'
-version = '1.0.0'
+# release = '1.0'
+release = '0.9'
+# version = '1.0.0'
+version = '0.9.0'
 
 # -- General configuration
 
